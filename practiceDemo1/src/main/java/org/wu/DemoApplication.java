810cbbee74b787c251b23e72d51508f0e0e9a7dd
Code_W <395485299@qq.com>
2018-05-08 20:47:37 +0800

    Origin
    Add to my github

---
+++ practiceDemo1/src/main/java/org/wu/DemoApplication.java
@@ -0,0 +1,19 @@
+package org.wu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    @RequestMapping("/")
+    public String index(){
+        return "home";
+    }
+}
